09fd247a4b038b7aed6288f622363c097d615e5c
Thomas Fercher (4AHEL) <thomas.fercher@htl-klu.at>
2021-06-10 20:30:09 +0200

    Button Theming & Modal

---
+++ lib/customwidgets/legendButton/legendButton.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webstore/customwidgets/legendButton/buttonStyle.dart';
+
+class LegendButton extends StatelessWidget {
+  final ButtonStyle? style;
+  final Widget text;
+  final Function onPressed;
+  final EdgeInsetsGeometry? margin;
+  final EdgeInsetsGeometry? padding;
+
+  const LegendButton({
+    Key? key,
+    this.style,
+    required this.text,
+    required this.onPressed,
+    this.margin,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin ?? const EdgeInsets.all(8.0),
+      child: TextButton(
+        onPressed: () => onPressed(),
+        child: Padding(
+          padding: padding ?? const EdgeInsets.all(8.0),
+          child: text,
+        ),
+        style: style ?? ButtonStyle(),
+      ),
+    );
+  }
+}
